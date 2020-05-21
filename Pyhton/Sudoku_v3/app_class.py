9016ea8376aabf2fc8861fbe3fe07083693f30d9
Jesrty <ravn1234567890@hotmail.com>
2020-05-22 00:54:44 +0200

    22-05-2020

--- Pyhton/Sudoku_v3/app_class.py
+++ Pyhton/Sudoku_v3/app_class.py
@@ -1,6 +1,6 @@
-import pygame
 import sys
 from settings import *
+import copy
 from buttonClass import *
 
 
@@ -9,15 +9,16 @@ class App:
         pygame.init()
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
-        self.grid = testBoard2
+        self.grid = emptyBoard.copy()
         self.selected = None
         self.mousePos = None
         self.state = "playing"
-        self.playingButtons = []
-        self.menuButtons = []
-        self.endButtons = []
+        self.button1 = None
+        self.button2 = None
+        self.button3 = None
+        self.lockedCell = []
         self.font = pygame.font.SysFont("arial", cellSize//2)
-        self.loadButtons()
+        self.load()
 
     def run(self):
         while self.running:
@@ -34,33 +35,69 @@ class App:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+
+            # user clicks
             if event.type == pygame.MOUSEBUTTONDOWN:
                 selected = self.mouseOnGrid()
                 if selected:
                     self.selected = selected
                 else:
-                    print("not on the board")
                     self.selected = None
+
+            # User types a key
+            if event.type ==pygame.KEYDOWN:
+                if self.selected != None and self.selected not in self.lockedCell:
+                    if self.isInt(event.unicode):
+                        # cell change
+                        self.grid[self.selected[1]][self.selected[0]] = int(event.unicode)
+
+            # User button click
+
+
+
+
+
+
+
+
 
     def playing_update(self):
         self.mousePos = pygame.mouse.get_pos()
-        for button in self.playingButtons:
-            button.update(self.mousePos)
+        mouseClick = pygame.mouse.get_pressed()
+
+        self.button1.update(self.mousePos)
+        self.button2.update(self.mousePos)
+        self.button3.update(self.mousePos)
+
+        if self.button1.click(self.mousePos, mouseClick):
+            self.solver()
+        elif self.button2.click(self.mousePos, mouseClick):
+            self.play()
+        elif self.button3.click(self.mousePos, mouseClick):
+            self.clear()
 
     def playing_draw(self):
         self.window.fill(WHITE)
 
-        for button in self.playingButtons:
-            button.draw(self.window)
+        self.button1.draw(self.window)
+        self.button2.draw(self.window)
+        self.button3.draw(self.window)
 
         if self.selected:
             self.drawSelected(self.window, self.selected)
+
+        self.shadeLockedCells(self.window, self.lockedCell)
+
 
         self.drawNumbers(self.window)
         self.drawGrid(self.window)
         pygame.display.update()
 
 ##### HELPER FUNKCTIONS #####
+
+    def shadeLockedCells(self, window, locked):
+        for cell in locked:
+            pygame.draw.rect(window, LOCKEDCELLCOLOUR, (cell[0]*cellSize+gridPos[0], cell[1]*cellSize+gridPos[1], cellSize, cellSize))
 
     def drawNumbers(self, window):
         for yidx, row in enumerate(self.grid):
@@ -88,7 +125,10 @@ class App:
         return ((self.mousePos[0]-gridPos[0])//cellSize, (self.mousePos[1]-gridPos[1])//cellSize)
 
     def loadButtons(self):
-        self.playingButtons.append(Button(20, 40, 100, 40))
+        self.button1 = Button(gridPos[0], 40, 100, 40, text="Solver")
+        self.button2 = Button(gridPos[0]+110, 40, 100, 40, text="Play")
+        self.button3 = Button(gridPos[0]+220, 40, 100, 40, text="Clear")
+
 
     def textToScreen(self, window, text, pos):
         font = self.font.render(text, False, BLACK)
@@ -97,6 +137,107 @@ class App:
         pos[0] += (cellSize-fontWidth)//2
         pos[1] += (cellSize-fontHeight)//2
         window.blit(font, pos)
+
+    def load(self):
+        self.loadButtons()
+
+    def isInt(self, string):
+        try:
+            int(string)
+            return True
+        except:
+            return False
+
+
+
+
+
+### SOLVER METODER ###
+
+    def _check_empty_square(self):
+        # check every place in the board for a 0 and returns the coordinates (in a tuple) or None if there is no 0
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
+                if self.grid[i][j] == 0:
+                    return (i, j)  # row, column
+        return None
+
+    def _check_number(self, n, pos):
+        # check if the row contains the number n
+        for i in range(len(self.grid[0])):
+            if self.grid[pos[0]][i] == n and pos[1] != i:
+                return False
+
+        # check if the column contains the number n
+        for i in range(len(self.grid)):
+            if self.grid[i][pos[1]] == n and pos[0] != i:
+                return False
+
+        # gives the square's upper left position
+        x0 = (pos[1] // 3) * 3
+        y0 = (pos[0] // 3) * 3
+        # check if the square contains the number n
+        for i in range(y0, y0 + 3):
+            for j in range(x0, x0 + 3):
+                if self.grid[i][j] == n and (i, j) != pos:
+                    return False
+        # if none of the test contains the number n return true
+        return True
+
+    def solver(self):
+        # use the _check_empty_square 2 get the position with a 0 (with out a number)
+        find = self._check_empty_square()
+        # if there is no 0 (empty position) the board is full and is a solution - else save the position in row and col
+        if not find:
+            return True
+        else:
+            row, col = find
+        # loops through 1-9 (every number in a sudoku)
+        for i in range(1, 10):
+            # if the number is valid in the sudoku, insert the number on the position
+            if self._check_number(i, (row, col)):
+                self.grid[row][col] = i
+
+                # will recursive call until the board is full (true) or loop though all the numbers
+                if self.solver():
+                    return True
+
+                # if no number match it back tracks and have 2 set the number too 0
+                self.grid[row][col] = 0
+        # return false if no solution
+        return False
+
+    def play(self):
+        self.grid = [[8, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 3, 6, 0, 0, 0, 0, 0],
+                    [0, 7, 0, 0, 9, 0, 2, 0, 0],
+                    [0, 5, 0, 0, 0, 7, 0, 0, 0],
+                    [0, 0, 0, 0, 4, 5, 7, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 3, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 6, 8],
+                    [0, 0, 8, 5, 0, 0, 0, 1, 0],
+                    [0, 9, 0, 0, 0, 0, 4, 0, 0]]
+        # setting locked cells from og board
+        for yidx, row in enumerate(self.grid):
+            for xidx, num in enumerate(row):
+                if num != 0:
+                    self.lockedCell.append((xidx, yidx))
+
+    def clear(self):
+        self.lockedCell.clear()
+        self.grid = [[0 for y in range(9)] for x in range(9)]
+
+    # Marcus hjælpe liste
+    # 1 button ikke klikker mange gange
+    # 2 self.grid = emptyboard vil fucke det op
+
+
+
+
+
+
+
+
 
 
 
--- Pyhton/Sudoku_v3/buttonClass.py
+++ Pyhton/Sudoku_v3/buttonClass.py
@@ -1,17 +1,21 @@
 import pygame
+from settings import *
+
 
 class Button:
-    def __init__(self, x, y, width, height, text = None, colour = (75, 75, 75), highlightedColour=(190, 190, 190), function = None, params = None):
+    def __init__(self, x, y, width, height, text=None, colour=(75, 75, 75), param=None, highlightedColour=(190, 190, 190)):
+        self.width = width
+        self.height = height
         self.image = pygame.Surface((width, height))
         self.pos = (x, y)
         self.rect = self.image.get_rect()
         self.rect.topleft = self.pos
         self.text = text
+        self.font = pygame.font.SysFont("arial", cellSize//2)
         self.colour = colour
         self.highlightedColour = highlightedColour
         self.highligthed = False
-        self.function = function
-        self.params = params
+        self.param = param
 
     def update(self, mouse):
         if self.rect.collidepoint(mouse):
@@ -22,5 +26,17 @@ class Button:
     def draw(self, window):
         self.image.fill(self.highlightedColour if self.highligthed else self.colour)
         window.blit(self.image, self.pos)
+
+        font = self.font.render(self.text, False, WHITE)
+        pos = (self.pos[0]+(self.width//4), self.pos[1]+(self.height//8))
+
+        window.blit(font, pos)
+
+    def click(self, mousePos, mouseClick):
+        if self.rect.collidepoint(mousePos) and mouseClick[0] == 1:
+            return True
+        return False
+
+
 
 
